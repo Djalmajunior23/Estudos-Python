273640c15881d51f3b1333eaacea8d683ca07e76
Djalmajunior23 <54447492+Djalmajunior23@users.noreply.github.com>
2021-01-16 20:42:37 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,9 @@
+num1=int(input("DIGITE UM NUMERO :"))
+num2=int(input("DIGITE UM OUTRO NUMERO :"))
+num3=int(input("DIGITE UM NUMERO :"))
+num4=int(input("DIGITE UM NUMERO :"))
+soma= num1+num2+num3+num4
+
+media=(soma/4)
+print("ESTE é O RESULTADO DA", soma)
+print("ESTE É O RESULTADO DA MÉDIA", media)
