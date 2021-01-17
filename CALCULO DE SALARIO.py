5ef01ca941b2432f54ab17a3f4e0ac93d2ba9048
Djalmajunior23 <54447492+Djalmajunior23@users.noreply.github.com>
2021-01-16 23:39:14 -0300

    Add files via upload
    O programa recebe as informações de um funcionário e faz o calculo do salário bruto.

---
+++ CALCULO DE SALARIO.py
@@ -0,0 +1,9 @@
+nome=input("DIGITE O NOME DO COLABORADOR: ")
+cargo=input("DIGITE O CARGO: ")
+departamento=input("INFORME O DEPARTAMENTO: ")
+horastrab=int(input("INFORME AS HORAS TRABALHADAS: "))
+salhora=float(input("INFORME O VALOR DA HORA DE TRABALHO: "))
+
+sal_mensal = horastrab * salhora
+print(sal_mensal)
+
